6a7c116f06ce8561a6281e194028ef6373b28900
entl <maxvorobyov01@gmail.com>
2024-10-17 21:39:41 +0100

    feat: implement chest creation

--- src/com/university/dungeon/room/RoomFactory.java
+++ src/com/university/dungeon/room/RoomFactory.java
@@ -1,7 +1,9 @@
 package com.university.dungeon.room;
 
+import com.university.gameElements.chests.Chest;
+import com.university.gameElements.chests.IChest;
 import com.university.gameElements.traps.ITrap;
-import com.university.gameElements.traps.MadScientists;
+import com.university.gameElements.traps.TrapFactory;
 import com.university.items.ItemFactory;
 import com.university.utils.commands.Direction;
 
@@ -9,39 +11,41 @@ import java.util.*;
 
 public class RoomFactory {
     private static final Random random = new Random();
-    private static final double TRAP_PROBABILITY = 0.2;
     private static final double ZERO_ITEM_PROBABILITY = 0.6;
     private static final double ONE_ITEM_PROBABILITY = 0.3;
     private static final double TWO_ITEM_PROBABILITY = 0.1;
+    private static final double CHEST_PROBABILITY = 0.1; // since only 1 chest there is no need to create factory
+    private static final double TRAP_PROBABILITY = 0.2;
     private static final ItemFactory itemFactory = new ItemFactory();
+    private static final TrapFactory trapFactory = new TrapFactory();
 
     public static Room createRoomFromCell(String cell, String name) {
         switch (cell.toUpperCase()) {
             case "W": {
                 // Wall
-                return new Room(name, false, false, false, true, null);
+                return new Room(name, false, false, false, true, null, null);
             }
             case "E": {
                 // Entrance
-                return new Room(name, true, false, false, false, null);
+                return new Room(name, true, false, false, false, null, null);
             }
             case "X": {
                 // Exit
-                return new Room(name, false, true, false, false, null);
+                return new Room(name, false, true, false, false, null, null);
             }
             case "T": {
                 // Treasure
-                return new Room(name, false, false, true, false, null);
+                return new Room(name, false, false, true, false, null, null);
             }
             case "R": {
                 // Regular Room
-                Room room = new Room(name, false, false, false, false, createTrap());
+                Room room = new Room(name, false, false, false, false, createTrap(), createChest());
                 addItemsToRoom(room);
                 return room;
             }
             default: {
                 // Handle unknown cell types as walls for safety
-                return new Room(name, false, false, false, true, null);
+                return new Room(name, false, false, false, true, null, null);
             }
         }
 
@@ -50,11 +54,15 @@ public class RoomFactory {
     private static ITrap createTrap() {
         double probability = random.nextDouble();
         if (probability < TRAP_PROBABILITY) {
-            if (random.nextBoolean()) {
-                return new MadScientists();
-            } else {
-                return new MadScientists();
-            }
+            return trapFactory.createRandomTrap();
+        }
+        return null;
+    }
+
+    private static IChest createChest() {
+        double probability = random.nextDouble();
+        if (probability < CHEST_PROBABILITY) {
+            return new Chest();
         }
         return null;
     }
