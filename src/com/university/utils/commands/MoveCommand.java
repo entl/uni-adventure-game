56230d8c227ef4941fe39cc86bc7ba6be0c5e617
entl <maxvorobyov01@gmail.com>
2024-11-15 14:18:26 +0000

    feat: print rooms content on move

--- src/com/university/utils/commands/MoveCommand.java
+++ src/com/university/utils/commands/MoveCommand.java
@@ -30,5 +30,6 @@ public class MoveCommand implements ICommand {
     @Override
     public void execute(GameContext context) {
         context.getPlayer().move(direction);
+        context.getPlayer().lookAround();
     }
 }
